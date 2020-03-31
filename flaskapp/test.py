07f8a936f85e79461a89848f971706c091a0930d
Rahul Das <rahuldas.rr81@gmail.com>
2020-03-31 22:20:10 +0530

    added comments

--- flaskapp/test.py
+++ flaskapp/test.py
@@ -4,29 +4,49 @@ import pandas as pd
 import cv2
 import matplotlib.pyplot as plt
 
+
+#initiates flask app
 app = Flask(__name__)
 
+
+#Create empty dataframe to fill with cordinates later
 df = pd.DataFrame()
 
 
+#url to send image file to javascript intenrally
 @app.route('/api/a')
 def image():
     return send_file('./Primavera.jpg',  mimetype='image/png')
 
 
+#home page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+#onclick submit button ajax query posts cord json object contaning all cordinates to this url
 @app.route('/postmethod', methods = ['POST'])
 def get_post_javascript_data():
+    
+    #loads json data in jsdata 
     jsdata = request.get_json()
+    
+    #makes df global variable so it can be accessed throughout this file
     global df
+    
+    #populates df dataframe with the recieved cordinates in jsdata 
     df = pd.DataFrame.from_dict(jsdata['cord'])
     print(df)
     
+    #just a return statement 
+    #no change needed in here
     return jsdata
 
+
+
+#url htttp://127.0.0.1/5000/df
+#access this url after clicking submit in the form to call the detect function defined in here
 @app.route('/df')
 def get_df():
     
@@ -37,32 +57,60 @@ def get_df():
 
 
 
+#detect function to load the image here in server in img variable
 
 def detect(data):
+    
+    #reads the img 
     img = cv2.imread('./Primavera.jpg',cv2.IMREAD_GRAYSCALE)
     print(img.shape)
 
-    img = cv2.resize(img,(911,700))
+    #as default width for canvas in html is 700
+    width =700
+    
+    #calculate height for new rescaled image as per width of canvas and maintaining image aspect ratio
+    height = width * (img.shape[0]/img.shape[1])
+    
+    #resize img with new dimensions
+    img = cv2.resize(img,(height,width))
 
     print(img.shape)
 
     import pytesseract
 
+    #creates a empty dictionary to store keys and extracted text values from pytesseraact
     texts= {}
     df = data
+    
+    #looping over each row in dataframe
+    #each row contains Key and cordinates of the region
     for i in range(df.shape[0]):
+        
+        #pull values from dataframe by [column_name] and [index] to respective variable 
         label,y1,y2,x1,x2 = df['keys'][i],df['starty'][i],int(df['starty'][i]+df['h'][i]),df['startx'][i],int(df['startx'][i]+df['w'][i])
+        
+        #crop the image with given the above cordinates
         roi = img[y1:y2,x1:x2]
+        
+        #print cordinates
         print(y1,y2,x1,x2)
+        
+        #Write the crop image to disk and check if it is the correct region 
         cv2.imwrite('new.jpg',roi)
-        # text = pytesseract.image_to_string(roi)
-        # texts[label] = text
+        
+        
+        #below is tesseract code which takes croped regoin of image as input and outputs texts in the image
+        #uncomment when achieved extracting correct region
+        ## text = pytesseract.image_to_string(roi)
+        ## texts[label] = text
 
     
 
-
+    #prints the texts dictionary containing Keys and extreacted texts
     # print(texts)
 
+    
+#Run the app
 app.run(debug=True)
 
 
