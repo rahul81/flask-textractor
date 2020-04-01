e9fe13de51121a3f411c9c2fc1bd98c2a8cc2d74
Rahul Das <rahuldas.rr81@gmail.com>
2020-04-01 18:19:37 +0530

    Update test.py

--- flaskapp/test.py
+++ flaskapp/test.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template, request, redirect, Response,jsonify,send_file
+from flask import Flask, render_template, request, redirect, Response,jsonify,send_file, flash, url_for,session,make_response
+from werkzeug.utils import secure_filename
+from flask_session import Session
+
 import random, json
 import pandas as pd 
 import cv2
 import matplotlib.pyplot as plt
-
+import os
 
 #initiates flask app
 app = Flask(__name__)
+
+UPLOAD_FOLDER = './static'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 
 
 #Create empty dataframe to fill with cordinates later
@@ -16,13 +25,59 @@ df = pd.DataFrame()
 #url to send image file to javascript intenrally
 @app.route('/api/a')
 def image():
-    return send_file('./Primavera.jpg',  mimetype='image/png')
-
+    print(image)
+    resp = make_response(send_file('./static/image/'+ image,  mimetype='image/png'))
+    resp.set_cookie('sessionID', '', expires=0)
+    return resp
 
 #home page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    resp = make_response(render_template('index.html'))
+    resp.set_cookie('sessionID', '', expires=0)
+    return resp
+
+
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/draw', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'img-file' not in request.files:
+            flash('No img file')
+            return redirect(request.url)
+        # if 'pdf-file' not in request.files:
+        #     flash('No pdf file')
+        #     return redirect(request.url)
+        img_file = request.files['img-file']
+        print("Inhere")
+        # pdf_file = request.files['pdf-file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if img_file.filename == '':
+            flash('No image file selected')
+            return redirect(request.url)
+        # if pdf_file.filename == '':
+        #     flash('No pdf file selected')
+        #     return redirect(request.url)
+        if img_file and allowed_file(img_file.filename):
+            print("In image fuct************************************")
+            global image
+            image = img_file.filename
+            
+            filename = secure_filename(img_file.filename)
+            img_file.save(os.path.join( "./static" + "/image", filename))
+        
+        return redirect(url_for('upload_file'))
+    resp = make_response(render_template('draw.html'))
+    resp.set_cookie('sessionID', '', expires=0)
+    return resp
+
 
 
 #onclick submit button ajax query posts cord json object contaning all cordinates to this url
